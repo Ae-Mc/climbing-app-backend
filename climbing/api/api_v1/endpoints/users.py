d69fbc24270e753ea1dfaa2602ff6de30ef7444a
Alexandr Makurin <ae_mc@mail.ru>
2022-01-15 05:54:20 +0300

    Use fastapi_users users router

--- climbing/api/api_v1/endpoints/users.py
+++ climbing/api/api_v1/endpoints/users.py
@@ -1,16 +1,14 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from fastapi.param_functions import Depends
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.engine import Result
+from climbing.api.deps import get_database
+from climbing.core.security import fastapi_users
+from climbing.schemas.user import User as PydanticUser
+from climbing.models.user import User
 
-from api.deps import get_database
-from crud import user
-import schemas
-
-
-router = APIRouter()
-
-schema = OAuth2PasswordBearer(tokenUrl="users")
+router = fastapi_users.get_users_router()
 
 
 # @router.post("/", response_model=schemas.User)
@@ -33,24 +31,26 @@ schema = OAuth2PasswordBearer(tokenUrl="users")
 #     return db_user
 
 
-@router.get("/", response_model=List[schemas.User])
-def read_users(
-    database: Session = Depends(get_database),
+@router.get("/", response_model=List[PydanticUser])
+async def read_users(
+    database: AsyncSession = Depends(get_database),
 ):
     """Список пользователей"""
-    return user.get_all(database)
+    statement = select(User)
+    result: Result = await database.execute(statement)
+    return result.all()
 
 
-@router.post("/", response_model=schemas.User)
-def add_user(
-    database: Session = Depends(get_database),
-    new_user: schemas.UserCreate = Depends(),
-):
-    """Создание нового пользователя. Должно быть доступно только
-    админимтратору"""
-    if user.get_by_username(database, new_user.username) is not None:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Пользователь с таким никнеймом уже существует",
-        )
-    return user.create(database=database, entity=new_user)
+# @router.post("/", response_model=schemas.User)
+# def add_user(
+#     database: Session = Depends(get_database),
+#     new_user: schemas.UserCreate = Depends(),
+# ):
+#     """Создание нового пользователя. Должно быть доступно только
+#     админимтратору"""
+#     if user.get_by_username(database, new_user.username) is not None:
+#         raise HTTPException(
+#             status_code=status.HTTP_409_CONFLICT,
+#             detail="Пользователь с таким никнеймом уже существует",
+#         )
+#     return user.create(database=database, entity=new_user)
