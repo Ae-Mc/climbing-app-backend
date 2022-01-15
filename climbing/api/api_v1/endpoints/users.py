9d6250abca8c9338e2dade6ece10e79a6138d181
Ae-Mc <ae_mc@mail.ru>
2022-01-16 00:50:46 +0300

    Change routes order in users router
    Remove dead commented code
    Set Get All Users route pseudonym
    Change Get All Users route path

--- climbing/api/api_v1/endpoints/users.py
+++ climbing/api/api_v1/endpoints/users.py
@@ -1,4 +1,6 @@
 from typing import List
+
+from fastapi import APIRouter
 from fastapi.param_functions import Depends
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio.session import AsyncSession
@@ -9,30 +11,14 @@ from climbing.core.security import fastapi_users
 from climbing.models.user import User
 from climbing.schemas.user import User as PydanticUser
 
-router = fastapi_users.get_users_router()
+router = APIRouter()
 
 
-# @router.post("/", response_model=schemas.User)
-# def login(
-#     database: Session = Depends(get_database),
-#     credentials: OAuth2PasswordRequestForm = Depends(),
-# ):
-#     """Вход"""
-#     db_user = user.authorize(
-#         database=database,
-#         username=credentials.username,
-#         password=credentials.password,
-#     )
-#     if db_user is None:
-#         raise HTTPException(
-#             detail="Неверный логин или пароль",
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     return db_user
-
-
-@router.get("/", response_model=List[PydanticUser])
+@router.get(
+    "",
+    response_model=List[PydanticUser],
+    name="users:all_users",
+)
 async def read_users(
     database: AsyncSession = Depends(get_database),
 ):
@@ -42,16 +28,4 @@ async def read_users(
     return result.all()
 
 
-# @router.post("/", response_model=schemas.User)
-# def add_user(
-#     database: Session = Depends(get_database),
-#     new_user: schemas.UserCreate = Depends(),
-# ):
-#     """Создание нового пользователя. Должно быть доступно только
-#     админимтратору"""
-#     if user.get_by_username(database, new_user.username) is not None:
-#         raise HTTPException(
-#             status_code=status.HTTP_409_CONFLICT,
-#             detail="Пользователь с таким никнеймом уже существует",
-#         )
-#     return user.create(database=database, entity=new_user)
+router.include_router(fastapi_users.get_users_router())
