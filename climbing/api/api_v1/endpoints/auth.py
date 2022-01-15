846ef37a0ca9ddde268767858eab98861e7e3563
Ae-Mc <ae_mc@mail.ru>
2022-01-16 00:51:18 +0300

    Add registration and verification endpoints to auth router

--- climbing/api/api_v1/endpoints/auth.py
+++ climbing/api/api_v1/endpoints/auth.py
@@ -1,4 +1,8 @@
-from climbing.core.security import fastapi_users, auth_backend
+from fastapi import APIRouter
 
+from climbing.core.security import auth_backend, fastapi_users
 
-router = fastapi_users.get_auth_router(backend=auth_backend)
+router = APIRouter()
+router.include_router(fastapi_users.get_auth_router(backend=auth_backend))
+router.include_router(fastapi_users.get_register_router())
+router.include_router(fastapi_users.get_verify_router())
