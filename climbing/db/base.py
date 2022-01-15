322eff795eac155f7889c2cd3571122ef3226293
Ae-Mc <ae_mc@mail.ru>
2022-01-16 00:42:31 +0300

    Add access token table to alembic

--- climbing/db/base.py
+++ climbing/db/base.py
@@ -1,11 +1,12 @@
 from climbing.models.file import File
 from climbing.models.route import Route
 from climbing.models.route_image import RouteImage
-from climbing.models.user import OAuthAccount, User
+from climbing.models.user import AccessToken, OAuthAccount, User
 
 from .base_class import Base
 
 __all__ = [
+    "AccessToken",
     "Base",
     "File",
     "OAuthAccount",
