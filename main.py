f97e6a18a0134e2e5502de1307d3e1a14ca27954
Ae-Mc <ae_mc@mail.ru>
2022-01-16 00:48:39 +0300

    Improve program launch configuration

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from uvicorn import run
+
 from climbing.main import app
 
-
-run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    try:
+        run("climbing.main:app", host="0.0.0.0", port=8000, reload=True)
+    except ImportError:
+        run(app, host="0.0.0.0", port=8000)
