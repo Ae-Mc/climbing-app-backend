c926a33e06eaf5968aac9c1481512f38d0ddf14a
Ae-Mc <ae_mc@mail.ru>
2022-01-16 05:50:31 +0300

    Add Route schemas to schemas module

--- climbing/schemas/__init__.py
+++ climbing/schemas/__init__.py
@@ -4,11 +4,15 @@ from .category import Category
 from .multipart_form_data_compatible_model import (
     MultipartFormDataCompatibleModel,
 )
+from .route import Route, RouteCreate, RouteUpdate
 from .user import User, UserCreate, UserDB, UserUpdate
 
 __all__ = [
     "Category",
     "MultipartFormDataCompatibleModel",
+    "Route",
+    "RouteCreate",
+    "RouteUpdate",
     "User",
     "UserCreate",
     "UserUpdate",
