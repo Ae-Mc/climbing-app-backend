c7b9a85350d78c3ef08c9af8e91e5ce459a18e6d
Ae-Mc <ae_mc@mail.ru>
2022-01-25 00:20:16 +0300

    Upgrade alembic's env to usee new SQLModel classes

--- alembic/env.py
+++ alembic/env.py
@@ -1,18 +1,12 @@
 import asyncio
-from inspect import getmodule
 from logging.config import fileConfig
-from typing import Type
 
-import fastapi_users_db_sqlalchemy
-import sqlalchemy
-import sqlalchemy_utils
-from sqlalchemy import Column, engine_from_config, inspect, pool
+from sqlalchemy import engine_from_config, pool
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import SQLModel
 
 from alembic import context
-from alembic.environment import MigrationContext
 from climbing.core.config import settings
-from climbing.db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -28,49 +22,12 @@ config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-
-def render_item(type_, obj: Type, autogen_context):
-    """Apply custom rendering for selected items."""
-
-    if type_ == "type":
-        if (
-            getmodule(obj).__package__.split(".")[0]
-            == sqlalchemy_utils.__package__
-        ):
-            autogen_context.imports.add("import sqlalchemy_utils")
-        elif (
-            getmodule(obj).__package__.split(".")[0]
-            == fastapi_users_db_sqlalchemy.__package__
-        ):
-            autogen_context.imports.add("import fastapi_users_db_sqlalchemy")
-
-    # default rendering for other objects
-    return False
-
-
-def compare_type(
-    migration_context: MigrationContext,
-    _inspected_column: Column,  # Column in db
-    metadata_column: Column,  # Column in local code
-    inspected_type: Type,
-    metadata_type: Type,
-):
-    if isinstance(metadata_type, sqlalchemy_utils.UUIDType):
-        inspector: sqlalchemy.engine.Inspector = inspect(
-            migration_context.bind
-        )
-        temp_table = metadata_column.table
-        inspector.reflect_table(temp_table, None)
-        return inspected_type == temp_table.columns[metadata_column.name].type
-    # default comparation
-    return None
 
 
 def run_migrations_offline():
@@ -93,8 +50,7 @@ def run_migrations_offline():
         dialect_opts={"paramstyle": "named"},
         compare_server_default=True,
         render_as_batch=True,
-        render_item=render_item,
-        compare_type=compare_type,
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -107,8 +63,7 @@ def do_run_migrations(connection):
         target_metadata=target_metadata,
         compare_server_default=True,
         render_as_batch=True,
-        render_item=render_item,
-        compare_type=compare_type,
+        compare_type=True,
     )
 
     with context.begin_transaction():
