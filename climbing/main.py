b24cb47fe7f2e0c8e39e419a4cb0a9b8d0f04005
Ae-Mc <ae_mc@mail.ru>
2024-09-23 22:12:26 +0000

    feat: add versionification

--- climbing/main.py
+++ climbing/main.py
@@ -3,12 +3,15 @@ from os import mkdir, path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi_versionizer import Versionizer
 
 from climbing.api.api_v1 import api_router as api_v1_router
+from climbing.api.api_v2 import api_router as api_v2_router
 from climbing.core.config import settings
 
 app = FastAPI(
     contact={"Автор": "Александр Макурин ae_mc@mail.ru|alexandr.mc12@gmail.com"},
+    version="v1",
 )
 
 app.include_router(api_v1_router)
@@ -16,7 +19,7 @@ app.include_router(api_v1_router)
 if not path.exists("media"):
     mkdir("media")
 static = StaticFiles(directory=settings.MEDIA_ROOT)
-app.mount("/media", static, name="media")
+app.include_router(api_v2_router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,3 +28,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+Versionizer(
+    app=app,
+    prefix_format="/api/v{major}",
+    semantic_version_format="{major}",
+    sort_routes=True,
+    include_version_docs=True,
+    include_versions_route=True,
+).versionize()
+
+app.mount("/media", static, name="media")
